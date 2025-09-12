63f6a08b57b71bbdff767206f41f2e65fe576283
Carlos. D <carlosgrodriguezz@hotmail.com>
2025-09-12 12:15:21 -0600

    Primer Commit, creacion de kt file

---
+++ app/src/main/java/com/example/practica_1_bandera/ui/theme/inicioscripts.kt
@@ -0,0 +1,14 @@
+package com.example.practica_1_bandera
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun InicioScreen(){
+
+    Box(){
+        Text(text = "Inicio Pantalla")
+    }
+
+}
