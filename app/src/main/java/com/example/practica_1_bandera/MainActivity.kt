527c53cf60b97e1b5cfe82ba694f867329ef5b6d
Carlos. D <carlosgrodriguezz@hotmail.com>
2025-09-17 22:12:25 -0600

    2do Commit, cree la columna en el Scaffold, y le agregue la funcion de franjanegra

--- app/src/main/java/com/example/practica_1_bandera/MainActivity.kt
+++ app/src/main/java/com/example/practica_1_bandera/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,7 +23,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             Practica_1_BanderaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ){
+                        franjanegra(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                        )
 
+                    }
             } 
         }
     }
